3fbcd15d9ef3823af26b57bd5a8c4a78208a0086
JaguarTordu <bastien.husler@gmail.com>
2022-03-10 10:09:33 +0100

    Initial Commit

---
+++ PubliMaps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PubliMaps
+//
+//  Created by Bastien HUSLER on 10/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PubliMaps/PubliMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PubliMapsApp.swift
+//  PubliMaps
+//
+//  Created by Bastien HUSLER on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct PubliMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
